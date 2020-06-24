6525e56eb760f0705468b32b155a8e98fd43d9e7
ziyuan <superboy753@yahoo.com.tw>
2020-06-25 02:16:24 +0800

    add django

---
+++ GossipSystem/Gossip/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from django.contrib import auth
+
+def login(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/index/')
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+    if user is not None and user.is_active:
+        auth.login(request, user)
+        return HttpResponseRedirect('/index/')
+    else:
+        return render(request, 'login.html', locals())
+
+def index(request):
+    return render(request, 'index.html')
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect('/index/')
